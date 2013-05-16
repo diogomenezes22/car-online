7cfebd4e2e0d72b0884d842c5c81dfeb5fdd0e5d
geraldo.estudos.trabalho <geraldo.estudos.trabalho@gmail.com>
2013-05-16 17:17:22 +0000

    Atualiacoes diogo

---
+++ carOnline/carOnline/carOnline/Controllers/CompraController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using carOnline.Models;
+
+namespace carOnline.Controllers
+{
+    public class CompraController : BaseController
+    {
+        //
+        // GET: /Compra/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Detalhes()
+        {
+            ViewBag.IdCarro = Request.QueryString[Ferramenta.QueryStrings.IdCarro] != null ? Request.QueryString[Ferramenta.QueryStrings.IdCarro].ToString() : null;
+
+            if ((ViewBag.IdCarro == null) || (!Ferramenta.IsInt32(ViewBag.IdCarro)))
+                Menssagem(ViewBag,"Por favor, escolha um carro");
+
+            return View();
+        }
+
+    }
+}
