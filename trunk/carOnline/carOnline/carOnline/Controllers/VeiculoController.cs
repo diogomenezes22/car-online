2bf90d15c8ec29d3d099641e44491f089c4b0706
geraldo.estudos.trabalho@gmail.com <geraldo.estudos.trabalho@gmail.com@26da73ef-b66f-3694-cb77-9eb24bbb95a5>
2013-05-16 20:11:37 +0000

    alterações geraldo

--- trunk/carOnline/carOnline/carOnline/Controllers/VeiculoController.cs
+++ trunk/carOnline/carOnline/carOnline/Controllers/VeiculoController.cs
@@ -315,8 +315,8 @@ namespace carOnline.Controllers
             if (tipoCaminho == "F")
             {
                // return @"C:/Users/Wellington/Documents/Visual Studio 2010/Projects/carOnline/carOnline/Content/images/veiculos/" + idVeiculo + "/";
-                //return @"C:/Users/geraldo.borges/Pictures/carOnline/Content/images/veiculos/" + idVeiculo + "/";
-                return @"C:/Users/geraldo/Pictures/CarOnline/carOnline/carOnline/carOnline/Content/images/veiculos/" + idVeiculo + "/";
+                return @"C:/Users/geraldo.borges/Pictures/CarOnline/Content/images/veiculos/" + idVeiculo + "/";
+                //return @"C:/Users/geraldo/Pictures/CarOnline/carOnline/carOnline/carOnline/Content/images/veiculos/" + idVeiculo + "/";
                 //return @"C:/Users/isaias/Desktop/CarOnline/Content/images/veiculos/" + idVeiculo + "/";
                 
             }
